6d8685cd81172b5a555b40bb7a9323370b2280dc
LeoGoldov <levfeb@gmail.com>
2025-05-31 21:17:23 +0300

    Update header.h

--- zadanie 1/header.h
+++ zadanie 1/header.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <iostream>
+class Product {
+private:
+    double calories100g;
+    double weight;
+
+public:
+    // Конструктор
+    Product(double c, double m);
+
+    // вычисление общей калорийности
+    double calculate();
+};
 
