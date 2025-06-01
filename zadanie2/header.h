01ce3c70298c46380d5bab2d1c83d1e504b947f3
LeoGoldov <levfeb@gmail.com>
2025-06-01 10:25:31 +0300

    Create header.h

---
+++ zadanie2/header.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Time {
+private:
+    int hour;
+    int minute;
+    int second;
+
+public:
+    // Конструктор по умолчанию (время 00:00:00)
+    Time();
+
+    // Конструктор с параметрами (часы, минуты, секунды)
+    Time(int h, int m, int s);
+
+    // Деструктор
+    ~Time();
+
+    long long totalSeconds() const;
+
+    void incrementSeconds(int secondsToAdd);
+
+    std::string toString() const; 
+};
